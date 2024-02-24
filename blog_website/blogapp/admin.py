ce66332f5c6e89e18a95c0d358947994f9746b07
Jhonneto003 <johneto003@gmail.com>
2024-02-24 11:51:25 -0800

    first commit

--- blog_website/blogapp/admin.py
+++ blog_website/blogapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import BlogPost
 # Register your models here.
+
+
+
+class MainAppAdmin(admin.ModelAdmin):
+    list_display= ['id', 'title', 'published_date']
+    list_display_links= ['id','title',]
+
+admin.site.register(BlogPost,  MainAppAdmin)
+
+admin.site.site_header='Django Blog Website Panel'
+admin.site.site_title= "Site portal"
+admin.site.index_title= "Site Administrative Panel"
